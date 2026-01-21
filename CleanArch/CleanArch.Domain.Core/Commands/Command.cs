326144ea29391fc74765799aabeeab5b694da2f9
RonaldoFM <43863628+RonaldoFM@users.noreply.github.com>
2026-01-20 23:30:49 -0300

    Add Infra.Bus And Domain.Core.

---
+++ CleanArch/CleanArch.Domain.Core/Commands/Command.cs
@@ -0,0 +1,14 @@
+﻿using CleanArch.Domain.Core.Events;
+
+namespace CleanArch.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Infra.Bus/InMemoryBus.cs
@@ -0,0 +1,23 @@
+﻿using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Core.Commands;
+using MediatR;
+
+namespace CleanArch.Infra.Bus
+{
+    public sealed class InMemoryBus : IMediatorHandler
+    {
+        private readonly IMediator _mediator;
+        public InMemoryBus(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        public async Task SendCommand<T>(T command) where T : Command
+        {
+            await _mediator.Send(command);
+        }
+        /*public async Task PublishEvent<T>(T @event) where T : Event
+        {
+            await _mediator.Publish(@event);
+        }*/
+    }
+}
